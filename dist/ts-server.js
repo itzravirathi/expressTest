5f581ad8b5771f4504c6b9eeabba9d3da0ce5708
Ravi Prakash Rathi <itzravirathi@yahoo.co.in>
2024-05-09 13:34:30 +0200

    Remove names from old commits. V2

--- dist/ts-server.js
+++ dist/ts-server.js
@@ -7,15 +7,28 @@ const express_1 = __importDefault(require("express"));
 // const express =  require("express");
 const app = (0, express_1.default)();
 const userRouter = require("./routes/users");
+const axios = require("axios");
+const restController = require("./controller/restController");
 app.listen(3000);
 app.set("view engine", "ejs");
 app.use(logger);
+app.use('/users', restController.router);
 app.use(express_1.default.static("public"));
 app.use(express_1.default.urlencoded({ extended: true }));
 app.get('/', (req, res) => {
+    var _a;
     console.log("Home page");
     res.status(200)
-        .render("index", { name: "Ravi Prakash Rathi" });
+        .render("index", { name: "Max Muster" });
+    const options = {
+        method: 'GET',
+        url: 'https://api2.binance.com/api/v3/ticker/24hr'
+    };
+    (_a = axios.request) === null || _a === void 0 ? void 0 : _a.call(axios, options).then(function (response) {
+        console.log(response.data);
+    }).catch(function (error) {
+        console.error(error);
+    });
 });
 app.get('/string', (req, res) => {
     console.log("string page");
@@ -34,8 +47,16 @@ app.get('/getJson', (req, res) => {
 });
 app.get('/getJsonWithSingleMiddleware', logger, (req, res) => {
     console.log("getJson page");
-    res.status(200)
-        .json({ message: "Hi there !!" });
+    try {
+        res.json({ message: "Hi there !!" })
+            .status(200)
+            .send();
+    }
+    catch (e) {
+        res.status(500)
+            .json({ error: "Sorry !! Something went." })
+            .send();
+    }
 });
 function logger(request, response, next) {
     console.log("Via TS Logger :" + request.originalUrl);
